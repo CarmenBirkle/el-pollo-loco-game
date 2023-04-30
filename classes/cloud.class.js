ddc17bf9e88e3cad1075153890af7d816e9c30e8
Carmen Birkle <carmen.birkle@iu-study.org>
2023-04-30 22:46:30 +0200

    cloud class

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,22 +1,25 @@
-class Cloud extends MovableObject { 
+/**
+ * Class to create clouds with moving animation and random position
+ *
+ */
+class Cloud extends MovableObject {
+  y = 40;
+  height = 250;
+  width = 500;
 
-    y = 40;
-    height = 250;
-    width = 500;
+  constructor() {
+    super().loadImage('img/5_background/layers/4_clouds/1.png');
+    this.x = 400 + Math.random() * 3000;
+    this.animate();
+  }
 
-    constructor() {
-        super().loadImage('img/5.Fondo/Capas/4.nubes/1.png');
-        this.x = Math.random() * 600;
-        this.animate();
-    }
-
-    /**
-     * change clouds position
-     * 
-     */
-    animate() {
-        setInterval(() => {
-            this.moveLeft();
-        }, 1000 / 60); 
-    }
+  /**
+   * Function to animate moving clouds
+   *
+   */
+  animate() {
+    setInterval(() => {
+      this.moveLeft();
+    }, 1000 / 60);
+  }
 }
