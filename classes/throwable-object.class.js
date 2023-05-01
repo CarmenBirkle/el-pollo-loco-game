878b41d530fda991a5d717fb97913cd75df313f6
Carmen Birkle <carmen.birkle@iu-study.org>
2023-05-01 21:05:09 +0200

    before delete - all pics are ok

--- classes/throwable-object.class.js
+++ classes/throwable-object.class.js
@@ -1,35 +1,35 @@
 class ThrowableOject extends MovableObject {
+  THROW_BOTTLE = [
+    'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+    'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+    'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+    'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png',
+  ];
 
-    THROW_BOTTLE = [
-        'img/6.botella/Rotacion/1.png',
-        'img/6.botella/Rotacion/2.png',
-        'img/6.botella/Rotacion/3.png',
-        'img/6.botella/Rotacion/4.png'
-    ];
+  constructor(x, y) {
+    super().loadImage(
+      'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png'
+    );
+    this.loadImages(this.THROW_BOTTLE);
+    this.x = x;
+    this.y = y;
+    this.height = 70;
+    this.width = 50;
+    this.trow();
+    this.animate();
+  }
 
-    
-    constructor(x, y) {
-        super().loadImage('img/6.botella/Rotacion/4.png');
-        this.loadImages(this.THROW_BOTTLE);
-        this.x = x;
-        this.y = y;
-        this.height = 70;
-        this.width = 50;
-        this.trow();
-        this.animate()
-    }
+  trow() {
+    this.speedY = 10;
+    this.applyGravity();
+    setInterval(() => {
+      this.x += 20;
+    }, 40);
+  }
 
-    trow() {
-        this.speedY = 10;
-        this.applyGravity();
-        setInterval(() => {
-            this.x += 20;
-        }, 40);
-    }
-
-    animate() {
-        setInterval(() => {
-                this.playAnimation(this.THROW_BOTTLE);
-        }, 100); 
-    }
+  animate() {
+    setInterval(() => {
+      this.playAnimation(this.THROW_BOTTLE);
+    }, 100);
+  }
 }
