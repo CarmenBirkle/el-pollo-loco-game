13cff77933d6b639e436a6137e98e6b66e23801a
Carmen Birkle <carmen.birkle@iu-study.org>
2023-05-01 21:22:49 +0200

    deletet

--- classes/throwable-object.class.js
+++ classes/throwable-object.class.js
@@ -5,25 +5,34 @@ class ThrowableOject extends MovableObject {
     'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
     'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png',
   ];
+  IMAGES_BOTTLES_SPLASH = [
+    'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+    'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+    'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+    'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+    'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+    'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png',
+  ];
 
   constructor(x, y) {
     super().loadImage(
       'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png'
     );
     this.loadImages(this.THROW_BOTTLE);
+    this.loadImages(this.IMAGES_BOTTLES_SPLASH);
     this.x = x;
     this.y = y;
     this.height = 70;
     this.width = 50;
-    this.trow();
+    this.throw();
     this.animate();
   }
 
-  trow() {
-    this.speedY = 10;
+  throw() {
+    this.speedY = 10; // Höhe des Wurfs 30
     this.applyGravity();
     setInterval(() => {
-      this.x += 20;
+      this.x += 20; // Weite des Wurfs  8
     }, 40);
   }
 
@@ -32,4 +41,15 @@ class ThrowableOject extends MovableObject {
       this.playAnimation(this.THROW_BOTTLE);
     }, 100);
   }
+
+  //   animate() {
+  //     setInterval(() => {
+  //       if (this.y > 250 || this.hurtEndboss()) {
+  //         this.playAnimation(this.IMAGES_BOTTLES_SPLASH);
+  //         this.x += 5;
+  //       } else {
+  //         this.playAnimation(this.IMAGES_BOTTLES_ROTATION);
+  //       }
+  //     }, 80);
+  //   }
 }
